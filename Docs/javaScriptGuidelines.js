ba8cc169b0ead9018b5e6a115e33ad44548cd74b
quintg1991@gmail.com <fglover@una.edu>
2019-02-26 13:07:37 -0600

    Initial commit

---
+++ Docs/javaScriptGuidelines.js
@@ -0,0 +1,35 @@
+
+// JAVASCRIPT STYLE Guidelines
+
+// Leave a space between comment syntax and the comment itself
+// Lines should end at line 80
+
+// A NOTE ON FUNCTIONS
+// Good code should be easy enough to follow without the use of comments.
+// That said, some snippets of code can benefit from comments when things get
+//  hairy. Use your own descretion when commenting complex code.
+
+// Descriptive variable names
+var descriptiveVarName = 5;
+
+// Brackets start on new line
+// Brief desctiption of what the function does
+function theFunction(var1, var2)
+{
+  // Two space indentation
+  if(condition)
+  {
+    // do something
+  }
+  else
+  {
+    // do somthing else
+  } 
+
+  // One empty space between unrelated blocks of code
+  for(var i = 0; i < descriptiveVarName; i++)
+  {
+    // do cool stuff here
+  }
+}
+
