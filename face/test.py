00aff0cc8fdb7b2baf58c2353937753e2cb25658
Mitchel Moon <mitchelmoon@HAL-9000.local>
2019-03-25 13:49:22 -0500

    adding test for face_recognition

---
+++ face/test.py
@@ -0,0 +1,11 @@
+import pathlib
+import face_recognition
+from PIL import Image
+
+if __name__ == '__main__':
+  src_img_path = ('series-2019-03-25_13-43-56' / 'image00000.jpg').resolve()
+  img = face_recognition.load_image_file(str(src_img_path))
+
+  face_recognition = face_recognition.face_locations(img)
+  print('Found: {} faces\n'.format(len(face_locations)))
+  
