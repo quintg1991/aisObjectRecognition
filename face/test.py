7090df798dea8401e8c4f37dd29f58ba27ce3025
Mitchel Moon <mitchelmoon@HAL-9000.local>
2019-03-25 13:52:18 -0500

    correcting variable name

--- face/test.py
+++ face/test.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
   src_img_path = (pathlib.Path('.') / 'series-2019-03-25_13-43-56' / 'image00000.jpg').resolve()
   img = face_recognition.load_image_file(str(src_img_path))
 
-  face_recognition = face_recognition.face_locations(img)
+  face_locations = face_recognition.face_locations(img)
   print('Found: {} faces\n'.format(len(face_locations)))
